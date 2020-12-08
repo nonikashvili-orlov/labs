8e5907d004e87b386c409f3d1dd955b3f07dcaa8
nonikashvili-orlov <brainbugged@yandex.ru>
2020-12-08 20:47:29 +0300

    lab1

---
+++ Lab1/Lab1/Cars.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab1
+{
+    public abstract class Transport
+    {
+        public string Color{ get; set; }
+
+        public int MaxSpeed { get; set; }
+
+        public int Weight { get; set; }
+
+        //абстрактный метод, который нужно обязательно определить в классе наследнике
+        public abstract void Move();
+
+        //виртуальный метод, его можно переопредилять, а можно и не переопределять
+        public virtual bool NeedOneDriver()
+        {
+            return true;
+        }
+    }
+    public class Car :Transport
+    {
+        public override void Move()
+        {
+            //логика для движения машины
+        }
+    }
+    public class Plane:Transport
+    {
+        public override void Move()
+        {
+            //логика для полета самолета
+        }
+        public override bool NeedOneDriver()
+        {
+            //переопределяем метод для определния количества людей за рулем
+            return false;
+        }
+}
+}
---
+++ Lab1/Lab1/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Plane plane = new Plane();
+            Random rnd = new Random();
+            plane.MaxSpeed = rnd.Next(500, 750);
+            plane.Weight = rnd.Next(500,900);
+            richTextBox1.Text = "Создался самолет с максимальной скоростью - " + plane.MaxSpeed + "\nи весом - " + plane.Weight;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Car car = new Car();
+            Random rnd = new Random();
+            car.MaxSpeed = rnd.Next(100, 150);
+            car.Weight = rnd.Next(100, 150);
+            richTextBox1.Text = "Создалась машина с максимальной скоростью - " + car.MaxSpeed + "\nи весом - " + car.Weight;
+        }
+    }
+}
